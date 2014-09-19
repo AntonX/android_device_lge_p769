0de398de602bb5dc7b403cc6526ee914ebdf178e
AntonX <antonx.git@gmail.com>
2014-09-19 11:22:21 -0400

    Modify camera fix for 4.4.4

--- releasetools.py
+++ releasetools.py
@@ -4,5 +4,4 @@ import sys
 import time
 
 def FullOTA_InstallEnd(info):
-  info.script.AppendExtra('symlink("/system/lib/libjhead.so", "/system/lib/libjhed.so");')
   info.script.AppendExtra('delete("/system/bin/auditd");')
